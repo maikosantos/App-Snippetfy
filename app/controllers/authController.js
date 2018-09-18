66050c33fff7f2355885b26518f31e9d908b0f89
Maiko <maikossmaster@gmail.com>
2018-09-18 16:02:48 -0300

    Autenticação.

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,28 +1,50 @@
-const { User } = require("../models");
-const bcrypt = require("bcrypt");
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   sigin(req, res) {
-    return res.render("auth/sigin");
+    return res.render('auth/sigin');
   },
 
   sigup(req, res) {
-    return res.render("auth/sigup");
+    return res.render('auth/sigup');
   },
 
   async register(req, res) {
     const { email } = req.body;
 
     if (await User.findOne({ where: { email } })) {
-      req.flash("error", "E-mail já cadastrado.");
-      return res.redirect("back");
+      req.flash('error', 'E-mail já cadastrado.');
+      return res.redirect('back');
     }
 
     const password = await bcrypt.hash(req.body.password, 5);
 
     await User.create({ ...req.body, password });
 
-    req.flash("success", "Usuário cadastrado com sucesso.");
-    return res.redirect("/");
-  }
+    req.flash('success', 'Usuário cadastrado com sucesso.');
+    return res.redirect('/');
+  },
+
+  async authenticate(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      req.flash('error', 'Usuário inexistente.');
+      return res.redirect('back');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      req.flash('error', 'Senha incorreta.');
+      return res.redirect('back');
+    }
+
+    req.session.user = user;
+
+    return req.session.save(() => {
+      res.redirect('app/dashboard');
+    });
+  },
 };
--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require('express');
 
 const routes = express.Router();
 
-const authController = require("./controllers/authController");
+const authController = require('./controllers/authController');
 
 routes.use((req, res, next) => {
-  res.locals.flashSuccess = req.flash("success");
-  res.locals.flashError = req.flash("error");
+  res.locals.flashSuccess = req.flash('success');
+  res.locals.flashError = req.flash('error');
   next();
 });
 
-routes.get("/", authController.sigin);
-routes.get("/sigup", authController.sigup);
+routes.get('/', authController.sigin);
+routes.get('/sigup', authController.sigup);
 
-routes.post("/register", authController.register);
+routes.post('/register', authController.register);
+routes.post('/authenticate', authController.authenticate);
 
 module.exports = routes;
