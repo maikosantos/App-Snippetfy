e90b95c1693df9df90fcb9eb4b90043b00a86593
Maiko <maikossmaster@gmail.com>
2018-09-19 10:31:22 -0300

    Error handler e middleware guest

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = express.Router();
 
 const authMiddlewares = require('./middlewares/auth');
+const guestMiddlewares = require('./middlewares/guest');
 
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
@@ -13,8 +14,8 @@ routes.use((req, res, next) => {
   next();
 });
 
-routes.get('/', authController.sigin);
-routes.get('/sigup', authController.sigup);
+routes.get('/', guestMiddlewares, authController.sigin);
+routes.get('/sigup', guestMiddlewares, authController.sigup);
 routes.get('/sigout', authController.sigout);
 
 routes.post('/register', authController.register);
@@ -22,5 +23,16 @@ routes.post('/authenticate', authController.authenticate);
 
 routes.use('/app', authMiddlewares);
 routes.get('/app/dashboard', dashboardController.index);
+
+routes.use((req, res) => res.render('errors/404'));
+
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('errors/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
 
 module.exports = routes;
