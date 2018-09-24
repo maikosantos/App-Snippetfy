a9640641064bce963417c3c4cf093cf0e537dbfa
Maiko <maikossmaster@gmail.com>
2018-09-24 17:30:06 -0300

    Visualização de snippets

--- app/controllers/snippetController.js
+++ app/controllers/snippetController.js
@@ -1,4 +1,4 @@
-const { Snippet } = require('../models');
+const { Snippet, Category } = require('../models');
 
 module.exports = {
   async store(req, res, next) {
@@ -12,6 +12,34 @@ module.exports = {
       req.flash('success', 'Snippet criado com sucesso');
 
       return res.redirect(`/app/categories/${categoryId}/snippets${snippet.id}`);
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async show(req, res, next) {
+    try {
+      const { categoryId, id } = req.params;
+
+      const categories = await Category.findAll({
+        include: [Snippet],
+        where: {
+          UserId: req.session.user.id,
+        },
+      });
+
+      const snippets = await Snippet.findAll({
+        where: { CategoryId: categoryId },
+      });
+
+      const snippet = await Snippet.findById(id);
+
+      res.render('snippets/show', {
+        activeCategory: categoryId,
+        categories,
+        snippets,
+        currentSnippet: snippet,
+      });
     } catch (error) {
       return next(error);
     }
