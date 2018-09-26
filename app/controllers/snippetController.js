df94f820fa72dc6fc16d969eea31cb038830536a
Maiko <maikossmaster@gmail.com>
2018-09-26 16:40:39 -0300

    Edição de snippet.

--- app/controllers/snippetController.js
+++ app/controllers/snippetController.js
@@ -11,7 +11,7 @@ module.exports = {
 
       req.flash('success', 'Snippet criado com sucesso');
 
-      return res.redirect(`/app/categories/${categoryId}/snippets${snippet.id}`);
+      return res.redirect(`/app/categories/${categoryId}/snippets/${snippet.id}`);
     } catch (error) {
       return next(error);
     }
@@ -40,6 +40,20 @@ module.exports = {
         snippets,
         currentSnippet: snippet,
       });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const snippet = await Snippet.findById(req.params.id);
+
+      await snippet.update(req.body);
+
+      req.flash('success', 'Snippet atualizado com sucesso');
+
+      return res.redirect(`/app/categories/${req.params.categoryId}/snippets/${snippet.id}`);
     } catch (error) {
       return next(error);
     }
